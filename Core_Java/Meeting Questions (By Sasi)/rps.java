d8fa12c5dc87597898a236f925a3d3e3c2af65b6
Uday-Saraswat <in.udaysaraswat@gmail.com>
2022-02-01 13:21:25 +0530

    rock paper scissors

--- Core_Java/Meeting Questions (By Sasi)/rps.java
+++ Core_Java/Meeting Questions (By Sasi)/rps.java
@@ -1,23 +1,25 @@
 
 public class rps {
     public static void main(String[] args) {
-        Game g1=new Game();
-        g1.rps("rock", "paper");
-        g1.rps("paper", "rock");
-       g1.rps("paper", "scissors");
-        g1.rps("scissors", "scissors");
-        g1.rps("scissors", "paper");
+        turn chance =new turn();
+        
+        chance.rps("rock", "paper");
+        chance.rps("paper", "rock");
+        chance.rps("paper", "scissors");
+        chance.rps("scissors", "scissors");
+        chance.rps("scissors", "paper") ;
+        
     }
 }
-class Game{
-    public void rps(String c1,String c2){
-        if(c1==c2){
+class turn{
+    public void rps(String player1,String player2){
+        if(player1==player2){
             System.out.println("TIE");
         }
-        if((c1=="rock" && c2=="paper")||(c1=="paper" && c2=="scissors")){
+        if((player1=="rock" && player2=="paper")||(player1=="paper" && player2=="scissors")){
             System.out.println("Player 2 wins");
         }
-        if((c1=="paper" && c2=="rock")||(c1=="scissors" && c2=="paper")){
+        if((player1=="paper" && player2=="rock")||(player1=="scissors" && player2=="paper")){
             System.out.println("Player 1 wins");
         }
 
